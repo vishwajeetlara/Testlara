fc5c762a12710186de447da0f455f5832abbfa4f
Vishwajeet Sahu <Vishwajeet_Sahu@Dellteam.com>
2021-03-28 10:47:24 +0530

    test method updated

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+class ClassA : ClassB
 
-namespace ConsoleApp9
 {
-    class Program
+
+    public void Display1()
+
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+        Console.WriteLine("ClassA Display1");
+
     }
+
+
+
+}
+
+
+
+class ClassB
+
+{
+
+    public int x = 100;
+
+    public void Display1()
+
+    {
+
+        Console.WriteLine("ClassB Display1");
+
+    }
+
+    public void Display2()
+
+    {
+
+        Console.WriteLine("ClassB Display2");
+
+    }
+
+}
+
+class Program
+
+{
+
+    static void Main(string[] args)        
+
+    {
+        ClassA a = new ClassA();
+
+    //classA a = new classB();
+
+    //classB a = new classA();
+
+    a.Display1();
+        Console.ReadLine();
+
+  }
+
 }
